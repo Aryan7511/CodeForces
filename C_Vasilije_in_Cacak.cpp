f1e371b446d434f21c73800087473b4eb8d95628
aryan <aryanmishra6174@gmail.com>
2024-09-22 20:58:10 +0530

    solved

---
+++ C_Vasilije_in_Cacak.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n; k++)
+#define pb push_back
+#define ll long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+#define PI 3.1415926535897932384626
+#define aryan ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+#define line cout << endl;
+ 
+ 
+void solve()
+{
+    ll n,k,x; cin>>n>>k>>x;
+    ll low = (k*(k+1))/2;
+    ll high = (k*(2*n-k+1))/2;
+    if(x>=low && x<=high) cout<<"YES";
+    else cout<<"NO";
+    line
+}
+ 
+int main()
+{ 
+    aryan;
+    clock_t start = clock();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    clock_t end = clock();
+    
+    #ifndef ONLINE_JUDGE
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << setprecision(10) << elapsed << endl;
+    #endif
+    return 0;
+}
