aa1f0835b34a86ca7aa9c289f49e62a52c954d05
aryan <aryanmishra6174@gmail.com>
2024-09-23 22:27:32 +0530

    solved

---
+++ A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n; k++)
+#define pb push_back
+#define ll long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+#define PI 3.1415926535897932384626
+#define aryan ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+#define line cout << endl;
+ 
+ 
+void solve()
+{
+    ll a,b,n; cin>>a>>b>>n;
+    ll ans=b;
+    for(int i=0;i<n;i++){
+      ll x; cin>>x;
+      ans+= min(a-1,x);
+    }
+    cout<<ans<<endl;
+}
+ 
+int main()
+{ 
+    aryan;
+    clock_t start = clock();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    clock_t end = clock();
+    
+    #ifndef ONLINE_JUDGE
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << setprecision(10) << elapsed << endl;
+    #endif
+    return 0;
+}
---
+++ A_Make_It_Zero.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n; k++)
+#define pb push_back
+#define ll long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+#define PI 3.1415926535897932384626
+#define aryan ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+#define line cout << endl;
+ 
+ 
+void solve()
+{
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    if(n&1) cout<<4<<endl<<1<<" "<<n-1<<endl<<1<<" "<<n-1<<endl<<n-1<<" "<<n<<endl<<n-1<<" "<<n<<endl;
+    else cout<<2<<endl<<1<<" "<<n<<endl<<1<<" "<<n<<endl;
+}  
+ 
+int main()
+{ 
+    aryan;
+    clock_t start = clock();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    clock_t end = clock();
+    
+    #ifndef ONLINE_JUDGE
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << setprecision(10) << elapsed << endl;
+    #endif
+    return 0;
+}
