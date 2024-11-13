7800f38fd60d4a270fa5e7bf7672b0f5f2b440b7
aryan <aryanmishra6174@gmail.com>
2024-11-13 11:28:21 +0530

    solved

---
+++ B_Multiply_by_2_divide_by_6.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n; k++)
+#define pb push_back
+#define ll long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+#define PI 3.1415926535897932384626
+#define aryan ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+#define line cout << endl;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<ll, ll> ml;
+typedef map<string, ll> msl;
+typedef map<ll, string> mls;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_map<string, ll> umsl;
+typedef unordered_map<ll, string> umls;
+typedef set<ll> sl;
+typedef set<pair<ll, ll>> spl;
+ 
+const ll mod = 1e9 + 7;
+ 
+void solve()
+{
+    int n; cin>>n;
+    if(n==1) {cout<<0<<endl; return;}
+    if(n%3!=0){cout<<-1<<endl; return;}
+
+    int threes=0,twos=0,num=n;
+    while(num%3==0){
+      threes++; num/=3;
+    }
+    while(num%2==0){
+      twos++; num/=2;
+    }
+    if(twos>threes || num>1){cout<<-1<<endl; return;}
+    int ans = 2*threes -twos;
+    cout<<ans<<endl;
+}
+ 
+int main()
+{ 
+    aryan;
+    clock_t start = clock();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    clock_t end = clock();
+    
+    #ifndef ONLINE_JUDGE
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << setprecision(10) << elapsed << endl;
+    #endif
+    return 0;
+}
